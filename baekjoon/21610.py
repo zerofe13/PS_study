d917190487fc04f5df7bd3ba8d7d06c341c8589b
zerofe <zerofe13@gmail.com>
2021-07-17 11:09:07 +0900

    비바리기 x

--- baekjoon/21610.py
+++ baekjoon/21610.py
@@ -7,10 +7,18 @@ N,M = map(int,input().split())
 array = [list(map(int,input().split())) for _ in range(N)]
 infoArr =[list(map(int,input().split()))for _ in range(M)]
 
-def move(d,s):
+def first(d,s):
     for _ in range(s):
         nx = 0 + dx[d]
         ny = N + ny[d]
         if nx == -1 : nx = N
         if ny == -1 : ny = N
-        
+    return nx,ny
+
+def second(x,y):
+    array[y][x] +=1
+    array[y+1][x] +=1
+    array[y][x-1] +=1
+    array[y+1][x-1] +=1
+
+
