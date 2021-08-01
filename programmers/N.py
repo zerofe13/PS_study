01b8f1fa986cb072480735e6f98d78e6db8ca4ac
zerofe <zerofe13@gmail.com>
2021-08-01 10:22:22 +0900

    N으로 표현

--- programmers/N.py
+++ programmers/N.py
@@ -1,1 +1,17 @@
-# N의로 표현
+def solution(N, number):
+    dp = [set() for _ in range(9)]
+    for i in range(1,9):
+        dp[i].add(int(str(N)*i))
+        for j in range(1,i):
+          for number1 in dp[j]:
+              for number2 in dp[i-j]:
+                  dp[i].add(number1 + number2)
+                  dp[i].add(number1 - number2)
+                  dp[i].add(number1 * number2)
+                  if number2 != 0:
+                      dp[i].add(number1 // number2)
+        if number in dp[i]:
+            return i
+    return -1
+
+print(solution(5,12))
