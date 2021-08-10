103f62a8a8937599218e94728d007f0d0b0ad0f7
zerofe <zerofe13@gmail.com>
2021-08-10 22:22:59 +0900

    통나무 옮기기

--- baekjoon/1938.py
+++ baekjoon/1938.py
@@ -1,58 +1,49 @@
 # 통나무 옮기기
+from collections import deque
+
 N = int(input())
 arr = [list(str,input()) for _ in range(N)]
+dx=[1,0,-1,0,1,-1,1,-1]
+dy=[0,1,0,-1,1,1,-1,-1]
 state = 0 # 0 수평 1 수직 
 e_state = 0 # 도착지점 상태 도착지점의 중심값과 형태가 통나무와 일치해야한다.
+b_pos = [] #통나무의 위치
+e_pos = [] # 도착지점 위치 
 check=[[[0 for col in range(N)] for row in range(N)] for depth in range(N)] #상태에 따른 방문 체크 
-def searchPos(arr):
-    pos = []
+
+def searchPos(arr): #위치 초기화함수
     for i in range(N):
         for j in range(N):
             if arr[i][j] == 'B':
-                pos.append([i,j])
-    return pos[1]
+                b_pos.append([i,j])
+            elif arr[i][j] == 'E':
+                e_pos.append([i,j])
 
-def up(state): #x,y 통나무 중심 위치
-    y,x = searchPos(arr)
-    if state == 0:    
-        if y-1 >=0 and arr[y-1][x-1]=='0' and arr[y-1][x]=='0' and arr[y-1][x+1]=='0':
-            for x in (x-1,x,x+1):
-                arr[y][x] = '0'
-                arr[y-1][x] = 'B'
-    else:
-        if y-2 >=0 and arr[y-2][x]=='0':
-            arr[y+1][x] ='0'
-            arr[y-2][x] ="B"
-def down(state): #x,y 통나무 중심 위치
-    y,x = searchPos(arr)
-    if state == 0:    
-        if y+1 <N and arr[y+1][x-1]=='0' and arr[y+1][x]=='0' and arr[y+1][x+1]=='0':
-            for x in (x-1,x,x+1):
-                arr[y][x] = '0'
-                arr[y+1][x] = 'B'
-    else:
-        if y+2 <0 and arr[y+2][x]=='0':
-            arr[y-1][x] ='0'
-            arr[y+2][x] ="B"
-def left(state): #x,y 통나무 중심 위치
-    x,y = searchPos(arr)
-    if state == 1:    
-        if x-1 >=0 and arr[x-1][y-1]=='0' and arr[x-1][y]=='0' and arr[x-1][y+1]=='0':
-            for x in (y-1,y,y+1):
-                arr[x][y] = '0'
-                arr[x-1][y] = 'B'
-    else:
-        if x-2 >=0 and arr[x-2][y]=='0':
-            arr[x+1][y] ='0'
-            arr[x-2][y] ="B"
-def right(state):
-    x,y = searchPos(arr)
-    if state == 1:    
-        if x+1 <N and arr[x+1][y-1]=='0' and arr[x+1][y]=='0' and arr[x+1][y+1]=='0':
-            for x in (y-1,y,y+1):
-                arr[x][y] = '0'
-                arr[x+1][y] = 'B'
-    else:
-        if x+2 <0 and arr[x+2][y]=='0':
-            arr[x][y-1] ='0'
-            arr[x][y+2] ="B"
+def checkState(posList): # 상태체크 
+    if posList[0][0]==posList[1][0]-1: # y ==y-1 수직
+        return 1
+    elif posList[0][1]==posList[1][1]-1: #x == x-1 수평
+        return 0 
+
+def veriticalMovePossible(y,x):  #수직 상태일때 이동가능
+    if arr[y+1][x] == "0" and arr[y-1][x] == "0":
+        return True
+    return False
+
+def horizonMovePossible(y,x): #수평상태일때 이동가능 
+    if arr[y][x+1] == "0" and arr[y][x-1] == "0":
+        return True
+    return False
+
+def turnPossible(y,x): #회전가능 
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dx[i]
+        if arr[nx][ny] != '0':
+            return False
+    return True
+
+searchPos(arr)
+# tree == target 가 되는 최단거리 (bfs로 구현)
+tree = [b_pos[1][0],b_pos[1][1],checkState(b_pos)] #[통나무 중심 y, 통나무 중심 x , 통나무 상태]
+target = [e_pos[1][0],e_pos[1][1],checkState(e_pos)] #[목표 중심 y, x, 상태]  
