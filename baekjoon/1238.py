9d6f1f759766a5bb567e4e66b919460a823f5ed8
zerofe13 <zerofe13@gmail.com>
2021-08-28 22:03:32 +0900

    파티

--- baekjoon/1238.py
+++ baekjoon/1238.py
@@ -1,30 +1,4 @@
-#전체경로 플루이드
-# import sys
-# input = sys.stdin.readline
-# INF = (1e9)
 
-# N,M,X = map(int,input().split()) # N = 학생수 M = 마을수 X = target
-# stdDist = [0]*(N+1)
-
-# graph = [[INF]*(N+1) for _ in range(N+1)]
-# for a in range(N+1):
-#     for b in range(N+1):
-#         if a==b:
-#             graph[a][b] = 0
-
-# for _ in range(M):
-#     a,b,c = map(int,input().split())
-#     graph[a][b] = c
-
-# for k in range(1,N+1):
-#     for a in range(1,N+1):
-#         for b in range(1,N+1):
-#             graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
-
-# for i in range(1,N+1):
-#     stdDist[i] = graph[i][X]+graph[X][i]
-# print(max(stdDist))
-# 플루이드 시간초과남 O(N^3)
 import sys
 import heapq
 input = sys.stdin.readline
