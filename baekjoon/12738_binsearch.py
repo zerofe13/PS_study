3dd04731f7dbcaa9f0ae6fba15d577102da5f580
zerofe13 <zerofe13@gmail.com>
2021-09-29 00:27:26 +0900

    가장 긴 증가하는 부분 수열3

---
+++ baekjoon/12738_binsearch.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+num_list = list(map(int,input().split()))
+dp = [-1e9-1]
+
+for num in num_list:
+    if dp[-1] <num :
+        dp.append(num)
+    else:
+        l,r = 0,len(dp)
+        while l<r:
+            mid = (l+r)//2
+            if dp[mid]<num:
+                l =mid+1
+            else:
+                r = mid
+        dp[r] = num
+print(len(dp)-1)
