d55ec1c34c9bdbe0ead309c81cbc7171b264c462
zerofe13 <zerofe13@gmail.com>
2021-10-26 21:48:20 +0900

    크레인 인형뽑기

---
+++ programmers/64061.py
@@ -0,0 +1,15 @@
+from collections import deque
+def solution(board, moves):
+    answer = 0
+    stack = deque()
+    for i in moves:
+        for y in range(len(board)):
+            if board[y][i-1] != 0:
+                stack.append(board[y][i-1])
+                board[y][i-1] = 0
+                if len(stack)>=2 and stack[-2] == stack[-1]:
+                    stack.pop()
+                    stack.pop()
+                    answer += 2
+                break
+    return answer
