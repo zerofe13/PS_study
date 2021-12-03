20933c2dd0fac5140a76dbc5ce1a0049077bb4f2
zerofe13 <zerofe13@gmail.com>
2021-12-03 23:19:30 +0900

    거리두기 확인히기

--- programmers/82301.py
+++ programmers/82301.py
@@ -30,5 +30,3 @@ def solution(places):
         else:
             answer.append(1)
     return answer
-
-print(solution([["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]))
