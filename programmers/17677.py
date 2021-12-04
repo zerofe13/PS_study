388cc6db2babea95ac64d7c8832d825d9f70cf8c
zerofe13 <zerofe13@gmail.com>
2021-12-05 03:42:59 +0900

    뉴스 클러스터링

--- programmers/17677.py
+++ programmers/17677.py
@@ -44,3 +44,23 @@ def solution(str1, str2):
     else:
         answer = math.trunc((len(inter_set)/len(union_set)) * 65536)
     return answer
+
+
+
+# import re
+# import math
+
+# def solution(str1, str2):
+#     str1 = [str1[i:i+2].lower() for i in range(0, len(str1)-1) if not re.findall('[^a-zA-Z]+', str1[i:i+2])]
+#     str2 = [str2[i:i+2].lower() for i in range(0, len(str2)-1) if not re.findall('[^a-zA-Z]+', str2[i:i+2])]
+
+#     gyo = set(str1) & set(str2)
+#     hap = set(str1) | set(str2)
+
+#     if len(hap) == 0 :
+#         return 65536
+#  각 원소 최솟값 , 최댓값 개수를 바로더해줌 Multiset 
+#     gyo_sum = sum([min(str1.count(gg), str2.count(gg)) for gg in gyo]) 
+#     hap_sum = sum([max(str1.count(hh), str2.count(hh)) for hh in hap])
+
+#     return math.floor((gyo_sum/hap_sum)*65536)
